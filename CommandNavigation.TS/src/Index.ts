f169dbe53588f0cff4e64d8265b17305d34549f1
ZoyuJ <ZoyuJ@hotmail.com>
2021-04-05 16:06:17 +0800

    fixed npm package name;remove useless Index file;

--- CommandNavigation.TS/src/Index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./Interfaces";
-export * from "./CommandNavigationx2";
-export * from "./CommandNavigationx4";
-export * from "./CommandManualNavigationx2";
-export * from "./CommandManualNavigationx4";
-export * from "./CommandStack";
---
+++ CommandNavigation/CommandsNavigationx2.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandsNavigation.CommandsNavigationx2 {
+  using CommandNavigation;
+
+  using System.Collections.Generic;
+
+  public class CommandNavigation<T> : Stack<T> where T : class, ICommandCtrl {
+
+
+
+
+  }
+
+}
